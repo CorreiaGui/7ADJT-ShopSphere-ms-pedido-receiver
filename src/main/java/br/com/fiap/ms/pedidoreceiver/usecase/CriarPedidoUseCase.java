151a0ce8e160854276eb7e8409fabe5f9990de73
larissahsilva <larissah_santos2010@hotmail.com>
2025-07-28 20:58:40 -0300

    correcao na fila

--- src/main/java/br/com/fiap/ms/pedidoreceiver/usecase/CriarPedidoUseCase.java
+++ src/main/java/br/com/fiap/ms/pedidoreceiver/usecase/CriarPedidoUseCase.java
@@ -1,13 +1,9 @@
 package br.com.fiap.ms.pedidoreceiver.usecase;
 
 import br.com.fiap.ms.pedidoreceiver.controller.json.PedidoRequestJson;
-import br.com.fiap.ms.pedidoreceiver.gateway.configuration.RabbitMqProducerConfiguration;
-import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import static br.com.fiap.ms.pedidoreceiver.gateway.configuration.RabbitMqProducerConfiguration.*;
 
 @Service
 public class CriarPedidoUseCase {
@@ -16,7 +12,6 @@ public class CriarPedidoUseCase {
     private RabbitTemplate rabbitTemplate;
 
     public void criarPedido(PedidoRequestJson pedidoRequestJson) {
-        rabbitTemplate.convertAndSend("exchange", "routingKey", pedidoRequestJson);
-    }
+        rabbitTemplate.convertAndSend("novo-pedido-queue", pedidoRequestJson);    }
 
 }
